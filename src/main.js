35ebcefad0a3ed531949696addb27d76bc0f2fe9
Kherbache Yacine <yacinepro69@gmail.com>
2021-09-04 12:57:17 +0200

    update paths
     for netlify build to pass

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router/index";
-import store from "./store/index";
+import router from "./router/index.js";
+import store from "./store/index.js";
 import axios from "axios";
 window.axios = axios;
 
